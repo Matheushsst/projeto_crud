cac9dea703fedd33f72c2119325349a6a58f73ba
Matheushsst <matheusilva670l@gmail.com>
2025-03-10 07:53:19 -0300

    Creation of the method "Listar"

--- backend/produtos/src/main/java/br/com/projetocrud/produtos/repository/ProductRepository.java
+++ backend/produtos/src/main/java/br/com/projetocrud/produtos/repository/ProductRepository.java
@@ -1,9 +1,11 @@
 package br.com.projetocrud.produtos.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import br.com.projetocrud.modelo.ProdutoModelo;
 
+@Repository
 public interface ProductRepository extends CrudRepository<ProdutoModelo, Long>{
     
 }
--- backend/produtos/src/main/java/br/com/projetocrud/produtos/service/ProductService.java
+++ backend/produtos/src/main/java/br/com/projetocrud/produtos/service/ProductService.java
@@ -1,5 +1,18 @@
 package br.com.projetocrud.produtos.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.projetocrud.modelo.ProdutoModelo;
+import br.com.projetocrud.produtos.repository.ProductRepository;
+
+@Service
 public class ProductService {
     
+    @Autowired
+    ProductRepository pr;
+
+    public Iterable<ProdutoModelo> listar(){
+        return pr.findAll();
+    }
 }
