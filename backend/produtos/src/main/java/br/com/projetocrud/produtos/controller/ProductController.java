6b2a374feb8837cf7c5a512ac7d30b927f99381d
Matheushsst <matheusilva670l@gmail.com>
2025-03-12 07:38:43 -0300

    Creation of the /listar route

--- backend/produtos/src/main/java/br/com/projetocrud/produtos/controller/ProductController.java
+++ backend/produtos/src/main/java/br/com/projetocrud/produtos/controller/ProductController.java
@@ -2,11 +2,25 @@ package br.com.projetocrud.produtos.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.projetocrud.modelo.ProdutoModelo;
+import br.com.projetocrud.produtos.service.ProductService;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
 @RequestMapping("/rota")
 public class ProductController {
+
+    @Autowired
+    private ProductService ps;
+
+    @GetMapping
+    public Iterable<ProdutoModelo> listar() {
+        return ps.listar();
+    }
+    
 
     @GetMapping
     public String rota(){
