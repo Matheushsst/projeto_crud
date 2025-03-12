8e2d8fc917764026450f449dfbbb73d149c3d5be
Matheushsst <matheusilva670l@gmail.com>
2025-03-12 07:35:00 -0300

    Creation of the response model class

---
+++ backend/produtos/src/main/java/br/com/projetocrud/modelo/ResponseModel.java
@@ -0,0 +1,13 @@
+package br.com.projetocrud.modelo;
+
+import org.springframework.stereotype.Component;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Component
+@Getter
+@Setter
+public class ResponseModel {
+    private String response;
+}
