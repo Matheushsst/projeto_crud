c0724f71fd3d4e77cf25e2cfa028c349f347d624
Matheushsst <matheusilva670l@gmail.com>
2025-05-13 10:58:01 -0300

    Register button

--- frontend/front/src/App.js
+++ frontend/front/src/App.js
@@ -5,8 +5,15 @@ import Table from './Table';
 
 function App() {
 
-const[RegisterBt] =useState(true);
+  const produto = {
+    codigo: null,
+    nome: '',
+    marca: ''
+  }
+
+const[RegisterBt, setRegisterBt] =useState(true);
 const[produtos, setProdutos] =useState([]);
+const[objProduto, setObjProduto] = useState(produto);
 
 useEffect(() => {
   fetch("http://localhost:8080/listar")
@@ -14,9 +21,34 @@ useEffect(() => {
       .then(retorno_convertido => setProdutos(retorno_convertido));
   }, []);
 
+  const aoDigitar = (e) => {
+    setObjProduto({...objProduto, [e.target.name]:e.target.value});
+  }
+
+  const cadastrar = () => {
+    fetch('http://localhost:8080/add', {
+      method: 'post',
+      body:JSON.stringify(objProduto),
+      headers:{
+        'Content-type': 'application/json',
+        'Accept': 'application/json'
+      }
+    })
+    .then(retorno => retorno.json())
+    .then(retorno_convertido =>{
+      if(retorno_convertido.mensage !== undefined){
+        alert(retorno_convertido.mensage);
+      }else{
+        setProdutos([...produtos, retorno_convertido]);
+        alert('Produto cadastrado com sucesso!')
+        setObjProduto(produto);
+      }
+    })
+  }
+
 return (    
       <div>
-        <Form button={RegisterBt}/>
+        <Form button={RegisterBt} eventoTeclado={aoDigitar} cadastrar={cadastrar} objProduto={objProduto}/>
         <Table vetor={produtos}/>
       </div>
   )
--- frontend/front/src/Form.js
+++ frontend/front/src/Form.js
@@ -1,22 +1,41 @@
-function Form(button){
-    return(
+function Form({ button, eventoTeclado, cadastrar, objProduto }) {
+    return (
         <form>
             <h2>Sistema Gestor de Produtos</h2>
-            <input className="form-control" type="text" placeholder="Nome"></input>
-            <input className="form-control" type="text" placeholder="Marca"></input>
+            <input
+                className="form-control"
+                type="text"
+                placeholder="Nome"
+                name="nome"
+                value={objProduto.nome}
+                onChange={eventoTeclado}
+            />
+            <input
+                className="form-control"
+                type="text"
+                placeholder="Marca"
+                name="marca"
+                value={objProduto.marca}
+                onChange={eventoTeclado}
+            />
             {
                 button
-                ?
-                <input className="btn btn-success" type="button" value="Cadastrar" />
-                :
-                <div>
-                    <input className="btn btn-warning" type="button" value="Cancelar" />
-                    <input className="btn btn-primary" type="button" value="Alterar" />
-                    <input className="btn btn-danger"  type="button" value="Remover" />
-                </div>
+                    ?
+                    <input
+                        className="btn btn-success"
+                        type="button"
+                        value="Cadastrar"
+                        onClick={cadastrar}
+                    />
+                    :
+                    <div>
+                        <input className="btn btn-warning" type="button" value="Cancelar" />
+                        <input className="btn btn-primary" type="button" value="Alterar" />
+                        <input className="btn btn-danger" type="button" value="Remover" />
+                    </div>
             }
         </form>
-    )
+    );
 }
 
 export default Form;
