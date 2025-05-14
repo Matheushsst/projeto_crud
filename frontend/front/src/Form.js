99a3b04883a863cb874083887143b4feb40b06b5
Matheushsst <matheusilva670l@gmail.com>
2025-05-14 10:40:53 -0300

    Added a background image to form

--- frontend/front/src/Form.js
+++ frontend/front/src/Form.js
@@ -1,40 +1,45 @@
 function Form({ button, eventoTeclado, cadastrar, objProduto, cancelar, remover, alterar}) {
     return (
-        <form>
-            <h2>Sistema Gestor de Produtos</h2>
-            <input
-                className="form-control"
-                type="text"
-                placeholder="Nome"
-                name="nome"
-                value={objProduto.nome}
-                onChange={eventoTeclado}
-            />
-            <input
-                className="form-control"
-                type="text"
-                placeholder="Marca"
-                name="marca"
-                value={objProduto.marca}
-                onChange={eventoTeclado}
-            />
-            {
-                button
-                    ?
+        <div className ='fundo-formulario'>
+            <div className="overlay">
+                <form className ='formulario'>
+                    <h2>Sistema Gestor de Produtos</h2>
+                
                     <input
-                        className="btn btn-success"
-                        type="button"
-                        value="Cadastrar"
-                        onClick={cadastrar}
+                        className="form-control"
+                        type="text"
+                        placeholder="Nome"
+                        name="nome"
+                        value={objProduto.nome}
+                        onChange={eventoTeclado}
                     />
-                    :
-                    <div>
-                        <input className="btn btn-warning" type="button" onClick={cancelar} value="Cancelar" />
-                        <input className="btn btn-primary" type="button" onClick={alterar} value="Alterar" />
-                        <input className="btn btn-danger" type="button" onClick={remover} value="Remover" />
-                    </div>
-            }
-        </form>
+                    <input
+                        className="form-control"
+                        type="text"
+                        placeholder="Marca"
+                        name="marca"
+                        value={objProduto.marca}
+                        onChange={eventoTeclado}
+                    />
+                    {
+                        button
+                            ?
+                            <input
+                                className="btn btn-success"
+                                type="button"
+                                value="Cadastrar"
+                                onClick={cadastrar}
+                            />
+                            :
+                            <div>
+                                <input className="btn btn-warning" type="button" onClick={cancelar} value="Cancelar" />
+                                <input className="btn btn-primary" type="button" onClick={alterar} value="Alterar" />
+                                <input className="btn btn-danger" type="button" onClick={remover} value="Remover" />
+                            </div>
+                    }
+                </form>
+            </div>
+        </div>
     );
 }
 
